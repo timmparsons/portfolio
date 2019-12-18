5412dfbf2d218b2dfd17a60c84388dc218fcf601
Tim Parsons <timmparsons85@gmail.com>
2019-12-18 07:07:21 -0700

    Updates

---
+++ precache-manifest.5eadc583972542e22b4132f14b5d673c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c3ed11357ab4ab692c58197a5fe2f4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0f87504629bc473b64d8",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "15f0fb8ff9ff8e9aac10",
+    "url": "/portfolio/static/js/2.ddc7936e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.ddc7936e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f87504629bc473b64d8",
+    "url": "/portfolio/static/js/main.b894639e.chunk.js"
+  },
+  {
+    "revision": "59bd250d2f9b2dea1b1d",
+    "url": "/portfolio/static/js/runtime-main.c064d8c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.ec5c76018c2ff1c996bce2cb2ce12c4b.js"
+  "/portfolio/precache-manifest.5eadc583972542e22b4132f14b5d673c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
